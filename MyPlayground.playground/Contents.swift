0574a22ccc25402c2e1058f7cfe6ca7f745bb02b
Eber Peña <eberg.pena@gmail.com>
2020-03-04 20:26:23 -0600

    Se definio la caracteristicas necesarias para asignar var o let

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,18 @@
 import UIKit
 
-var str = "Hello, playground"
-var var2 = "Hola MUndo"
+var str = "Hello, codebox"
+
+
+// Constantes y Variables
+// Una variable puede cam
+
+str = " Hola Mundo"
+
+
+// Esta es la constante
+
+let StringConstante = "Hola mundo Cruel"
+
+//NO ADMITE MUTABILIDAD StringConstante = "Hola mundo Bonito"
+
+
