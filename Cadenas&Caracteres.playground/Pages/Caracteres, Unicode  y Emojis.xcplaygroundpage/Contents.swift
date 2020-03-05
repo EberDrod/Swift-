5f5ed127893f1b8af537f11306628cc54d001fc3
Eber Peña <eberg.pena@gmail.com>
2020-03-04 21:52:58 -0600

    Para crear emoji como variable un ctrl+cmd+space, ya que usa UNICODE

---
+++ Cadenas&Caracteres.playground/Pages/Caracteres, Unicode  y Emojis.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//Caracter
+let myCaracter:Character = "\u{E9}"
+let myCharacter2 = "\u{24}"
+
+//Caracteres especiales (\n, \r, \t, \0, \')
+let myString = "Bienvenidos al curso de Swift"
+
+// Emojis
+
+let myemoji = "😉"
+
---
+++ Cadenas&Caracteres.playground/Pages/Tipo de dato String.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+//String en una línea
+let myString = "Bienvenido al curso de Swift"
+
+//String en varías líneas
+let myMultipleString = """
+Bienvenido al curso Swift
+
+Espero te parezca interesante el String
+"""
+
+
+//String en varías líneas unicamente en código
+let myFalseMultipleString = """
+Bienvenido al curso Swift\
+\
+Espero te parezca interesante el String
+"""
+
+
+//String vacío
+
+let myEmpityString = ""
+let myEmptyString2 = String()
+
+
