42b491e31f7e3794fe5cda2b5b57b22f8f180f23
Eber Peña <eberg.pena@gmail.com>
2020-03-04 20:55:47 -0600

    Se ejemplifican los typos de datos verificar DATA TYPES DEFINITION

---
+++ Declaración_Multiple.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+//USAR OPTION EN LA VARIABLE PARA VER LA DOCUMENTACIÓN DE LA VARIABLE
+var str = "Hello, playground"
+
+let miNombreCompleto = "Eber Peña"
+let miFechaDeNacimiento = "1-1-1990"
+let miLugarDeNacimiento = "Mexico"
+let puntoCongelacion = 0
+
+
+//Tipos de Datos Comunes:
+//VERIFICAR DATA TYPES DEFINITION
+
+//Enteros:
+
+var x = 18
+//Flotantes:
+
+var y = 18.8
+
+
+//Operadores aritmeticos + - * /
+
+var sumaEnteros =  x + 2
+
+var sumaFlotantes = y + 2.2
+
+var suma = Double(x) + y
+
+var division = Float (x) / 5
+
+//Asignación Implicita y Explicita 
